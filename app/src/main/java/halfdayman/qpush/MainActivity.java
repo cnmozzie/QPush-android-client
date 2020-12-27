8cf9b2a8feeb0c78b573f6b3fd0278cde39e3d2a
Guo Weixuan <cnmozzie@gmail.com>
2020-12-27 23:06:52 +0800

    Add Toolbar

--- app/src/main/java/halfdayman/qpush/MainActivity.java
+++ app/src/main/java/halfdayman/qpush/MainActivity.java
@@ -1,6 +1,7 @@
 package halfdayman.qpush;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
