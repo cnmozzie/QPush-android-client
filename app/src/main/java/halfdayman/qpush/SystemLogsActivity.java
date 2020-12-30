b4c953426679c621392dd89da416dd89d9d994f1
Guo Weixuan <cnmozzie@gmail.com>
2020-12-30 16:29:45 +0800

    Adjust toolbar UI

--- app/src/main/java/halfdayman/qpush/SystemLogsActivity.java
+++ app/src/main/java/halfdayman/qpush/SystemLogsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,11 +20,18 @@ public class SystemLogsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_logs);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        myToolbar.setTitle("System Logs");
-        setSupportActionBar(myToolbar);
         mLogView = (TextView) findViewById(R.id.log);
         QPushApplication.setSystemLogsActivity(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);       //加载图标
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {   //为图标设置监听器
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
