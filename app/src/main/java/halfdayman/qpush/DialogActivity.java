4e9c91b97e635c677aa3b03412923640e897ee84
Guo Weixuan <cnmozzie@gmail.com>
2021-01-01 17:20:53 +0800

    get data from server

--- app/src/main/java/halfdayman/qpush/DialogActivity.java
+++ app/src/main/java/halfdayman/qpush/DialogActivity.java
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,44 +43,45 @@ public class DialogActivity extends AppCompatActivity {
 
 
         initMessages();
-        mAdapter=new MessageAdapter(this,mDatas);
 
-        mMsgList = findViewById(R.id.message_list);
-        mMsgList.setAdapter(mAdapter);
     }
 
     // 初始化数据
     private void initMessages() {
-        for(int i=0;i<5;i++){
-            Message message = new Message(1+i*5,
-                    "12月27日 上午11:56",
-                    "如何才能不错过人工智能的时代？",
-                    "下一个时代就是机器学习的时代，慕课网发大招，与你一起预见未来！");
-            mDatas.add(message);
+        String url ="https://www.pkumozzie.cn/blade-trap/get?table=messages&orderBy=create_time";
 
-            message = new Message(2+i*5,
-                    "12月27日 上午11:56",
-                    "关于你的面试、实习心路历程",
-                    "奖品丰富，更设有参与奖，随机抽取5名幸运用户，获得慕课网付费面试课程中的任意一门！");
-            mDatas.add(message);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
 
-            message = new Message(3+i*5,
-                    "12月27日 上午11:56",
-                    "狗粮不是你想吃，就能吃的！",
-                    "你的朋友圈开始了吗？一半秀恩爱，一半扮感伤！不怕，还有慕课网陪你坚强地走下去！！");
-            mDatas.add(message);
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        JSONObject jsonObject = null;
+                        for(int i = 0;i < response.length();i++){
 
-            message = new Message(4+i*5,
-                    "12月27日 上午11:56",
-                    "前端跳槽面试那些事儿",
-                    "工作有几年了，项目偏简单有点拿不出手怎么办？ 目前还没毕业，正在自学前端，请问可以找到一份前端工作吗，我该怎么办？");
-            mDatas.add(message);
+                            try {
+                                jsonObject = (JSONObject) response.get(i);
+                                Message message = new Message(i,
+                                        jsonObject.getString("create_time"),
+                                        jsonObject.getString("user_name"),
+                                        jsonObject.getString("text"));
+                                mDatas.add(message);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        mAdapter=new MessageAdapter(DialogActivity.this, mDatas);
 
-            message = new Message(5+i*5,
-                    "12月27日 上午11:56",
-                    "图解程序员怎么过七夕？",
-                    "哈哈哈哈，活该单身25年！");
-            mDatas.add(message);
-        }
+                        mMsgList = findViewById(R.id.message_list);
+                        mMsgList.setAdapter(mAdapter);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("Volley Error","That didn't work!");
+                    }
+                });
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
     }
 }
